97fd566a5f17ddefe2ee5f4abfc1e1bd0768fb22
ColdCipri <colder.ciprian@gmail.com>
2020-01-08 15:58:00 +0200

    Added images and 2 folders(for homepage and part 3 of app)

---
+++ Nivel3/index.js
@@ -0,0 +1,9 @@
+var questions =	["Care este cea mai apropiata planeta fata de Soare?" : "../Audio/Care este cea mai apropiata.m4a",
+				 "Care este cea mai fierbinte planeta din Sistemul Solar?",
+				 "Care este planeta acoperita in mare parte de apa?",
+				 "Care este \"Planeta Rosie\"?",
+				 "Care este cea mai mare planeta?",
+				 "Care este planeta cunoscuta pentru inelele sale?",
+				 "Care este cea mai rece planeta?",
+				 "Care este cea mai departata planeta fata de Soare?"
+				];
